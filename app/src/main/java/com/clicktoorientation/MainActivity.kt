69e4bb3cdeb65267d396b0d38d55bf7c5e923df3
이 종원 <jong12em@naver.com>
2020-03-10 20:41:54 +0900

    [기능] 실행시 orientation 반전
    1. WRITE_SYSTEM 퍼미션 확인 및 받아오기
    
    2. 현재 orientation 확인
    
    3. 시스템에 바뀐 orientation set 해주기

---
+++ app/src/main/java/com/clicktoorientation/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.clicktoorientation
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.System.canWrite(this)) {
+                changeOrientation()
+            } else {
+                val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+
+    }
+
+    private fun changeOrientation(){
+        val orientation = resources.configuration.orientation
+        Log.i("!!", "$orientation")
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Settings.System.putInt(
+                contentResolver,
+                Settings.System.USER_ROTATION,
+                0
+            )
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Settings.System.putInt(
+                contentResolver,
+                Settings.System.USER_ROTATION,
+                1
+            )
+        }
+        finish()
+    }
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.System.canWrite(this)) {
+                changeOrientation()
+            }else
+            {
+                finish()
+            }
+        }
+    }
+}
